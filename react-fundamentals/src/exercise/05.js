b5d5d322e7e6be54b2d13a9a18fe56a572752f0c
Shubham Singh <shubham88fru@yahoo.com>
2022-06-26 13:07:58 +0530

    forms in react

--- react-fundamentals/src/exercise/05.js
+++ react-fundamentals/src/exercise/05.js
@@ -1,18 +1,5 @@
-// Styling
-// http://localhost:3000/isolated/exercise/05.js
-
 import * as React from 'react'
 import '../box-styles.css'
-
-// 🐨 add a className prop to each div and apply the correct class names
-// based on the text content
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-// 💰 each of the elements should have the "box" className applied
-
-// 🐨 add a style prop to each div so their background color
-// matches what the text says it should be
-// 🐨 also use the style prop to make the font italic
-// 💰 Here are available style attributes: backgroundColor, fontStyle
 
 function Box({size, style, ...otherProps}) {
   return (
--- react-fundamentals/src/exercise/06.js
+++ react-fundamentals/src/exercise/06.js
@@ -1,31 +1,43 @@
-// Basic Forms
-// http://localhost:3000/isolated/exercise/06.js
-
 import * as React from 'react'
+import {useState} from 'react'
+import {useRef} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const nameRef = useRef(null)
+  const [error, setError] = useState('')
+  const [input, setInput] = useState('')
 
-  // 🐨 add the onSubmit handler to the <form> below
+  function handleSubmit(e) {
+    e.preventDefault()
+    //const ip = e.target[0].value --> // get value from event object
+    const ip = nameRef.current.value //get value using ref
+    onSubmitUsername(ip)
+  }
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  function handleInputChange(e) {
+    const ip = e.target.value
+
+    // const isValid = ip === ip.toLowerCase()
+    // setError(isValid ? null : 'Input should be all lowercase')
+
+    setInput(ip.toLowerCase())
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input
+          onChange={handleInputChange}
+          value={input}
+          ref={nameRef}
+          type="text"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={error} type="submit">
+        Submit
+      </button>
+      <p>{error}</p>
     </form>
   )
 }
